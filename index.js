2f5a5b11a318f4ca68d613942e5501e38a70f1d2
Marcelo Aguila <web@croop.cl>
2019-02-18 17:45:21 -0300

    linted

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 require('dotenv').config();
 const schedule = require('node-schedule');
-const twit = require("twit");
-const moment = require("moment");
-const fetch = require("node-fetch");
+const Twit = require('twit');
+const moment = require('moment');
+const fetch = require('node-fetch');
 
-schedule.scheduleJob('0 * * * *', function(){
-  getWeather();
+const t = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+  timeout_ms: 60 * 1000,
 });
 
-var t = new twit({
-  consumer_key:         process.env.consumer_key,
-  consumer_secret:      process.env.consumer_secret,
-  access_token:         process.env.access_token,
-  access_token_secret:  process.env.access_token_secret,
-  timeout_ms:           60*1000
-})
-
-function getWeather(){
-  var urlApi='https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=sp&units=metric&q=Punta+Arenas';
-  var headersOW= { headers: { 'X-RapidAPI-Key': process.env.rapidApi } };
-  var timestamp = moment().tz("America/Punta_Arenas").format('LT');
+function getWeather() {
+  const urlApi = 'https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=sp&units=metric&q=Punta+Arenas';
+  const headersOW = { headers: { 'X-RapidAPI-Key': process.env.rapidApi } };
+  const timestamp = moment().tz('America/Punta_Arenas').format('LT');
   fetch(urlApi, headersOW)
-      .then(res => res.json())
-      .then(json => {
-          var status = `El #tiempo en #puq: ${json.main.temp}º , ${json.weather[0].description}, humedad del ${json.main.humidity}% a las ${timestamp}`;
-          console.log(status);
-          t.post('statuses/update', { status: status }, function(err, data, response) {
-              console.log(data)
-          })
-      })
-      .catch(err => console.error(err));
+    .then(res => res.json())
+    .then((json) => {
+      const status = `El #tiempo en #puq: ${json.main.temp}º , ${json.weather[0].description}, humedad del ${json.main.humidity}% a las ${timestamp}`;
+      console.log(status);
+      t.post('statuses/update', { status }, (err, data) => {
+        console.log(data);
+      });
+    })
+    .catch(err => console.error(err));
 }
+getWeather();
+schedule.scheduleJob('0 * * * *', () => {
+  getWeather();
+});
